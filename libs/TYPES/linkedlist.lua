a2a3a1bc2fc94a4a8ffbc1fbba13f9e451d79155
Kyando2 <amehikoji@gmail.com>
2022-03-09 17:57:17 -0500

    Implement a linked list

--- libs/TYPES/linkedlist.lua
+++ libs/TYPES/linkedlist.lua
@@ -1,3 +1,30 @@
-return setmetatable({}, {__call = function(...)
-
+return setmetatable({}, {__call = function()
+    local headnode = {VAL = nil, NEXT = nil}
+    function GetNodeAtIndex(t,k, stopifnil)
+        if k == 1 then
+            return t
+        end
+        local cnode = t
+        for x=2,k do
+            if rawget(cnode, "NEXT") ~= nil then
+                cnode = rawget(cnode, "NEXT")
+            else 
+                if stopifnil then
+                    return nil
+                end
+                rawset(cnode, "NEXT", {VAL = nil, NEXT = nil})
+                cnode = rawget(cnode, "NEXT")
+            end
+        end
+        return cnode
+    end
+    return setmetatable(headnode, {
+        __newindex=function (t,k,v)
+            rawset(GetNodeAtIndex(t,k), "VAL", v)
+        end,
+        __index=function (t,k)
+            return rawget(GetNodeAtIndex(t,k), "VAL")
+        end,
+    })
 end})
+
