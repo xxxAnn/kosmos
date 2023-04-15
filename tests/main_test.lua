98c93798337242482acb1e3efa53ae8f1677511d
キャンド <amehikoji@gmail.com>
2023-04-15 16:10:11 -0400

    add comment

--- tests/main_test.lua
+++ tests/main_test.lua
@@ -2,6 +2,7 @@ local kosmos = require("kosmos")
 -- Creates a component type with one field called 'x' which must have a type of 'table'
 local my_comp = kosmos.components.new("my_comp", {x = "table"})
 -- This should obtain the type that was just created through its id
+-- KOSMOS.components.get is equivalent to KOSMOS.components.new 
 local my_comp_again = kosmos.components.get("my_comp")
 -- Creates a component of type 'my_comp'
 local my_comp_value = my_comp_again{x = {}}
